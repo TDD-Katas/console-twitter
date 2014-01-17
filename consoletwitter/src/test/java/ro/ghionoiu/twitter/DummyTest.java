4829aa7a9cdf759ea470654ab12614ea9e994fb0
Iulian Ghionoiu <iulian.ghionoiu@exenne.ro>
2014-01-17 15:48:09 +0200

    Refactored the tests for detecting command type

--- consoletwitter/src/test/java/ro/ghionoiu/twitter/DummyTest.java
+++ consoletwitter/src/test/java/ro/ghionoiu/twitter/DummyTest.java
@@ -7,6 +7,8 @@ package ro.ghionoiu.twitter;
 import org.junit.Test;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.CoreMatchers.is;
+import org.hamcrest.Matcher;
+import static org.junit.Assert.assertThat;
 
 /**
  *
@@ -16,6 +18,8 @@ public class DummyTest {
     public static final String KEYWORD_ARROW = "->";
     public static final String KEYWORD_FOLLOWS = "follows";
     public static final String KEYWORD_WALL = "wall";
+
+
     
     public static final String COMMAND_POST = "post";
     public static final String COMMAND_FOLLOW = "follow";
@@ -24,51 +28,53 @@ public class DummyTest {
     
     @Test
     public void a_line_that_contains_keyword_arrow_is_a_post_command() {
-        String line = "Alice "+KEYWORD_ARROW+" ole";
-        
-        String type = "";
-        if(line.contains(KEYWORD_ARROW)) {
-            type = COMMAND_POST;
-        }
-        
-        assertThat(type, is(COMMAND_POST));
+        assertThatCommandTypeFor("Alice -> Hello !", is(COMMAND_POST));
     }
     
     @Test
     public void a_line_that_contains_keyword_follows_is_a_follow_command() {
-        String line = "Alice "+KEYWORD_FOLLOWS+" Bob";
-        
-        String type = "";
-        if(line.contains("follows")) {
-            type = "follow";
-        }
-        
-        assertThat(type, is(COMMAND_FOLLOW));
+        assertThatCommandTypeFor("Alice follows Bob", is(COMMAND_FOLLOW));
     }
     
     @Test
     public void a_line_that_contains_keyword_wall_is_a_wall_command() {
-        String line = "Alice "+KEYWORD_WALL;
-        
-        String type = "";
-        if(line.contains(KEYWORD_WALL)) {
-            type = COMMAND_WALL;
-        }
-        
-        assertThat(type, is(COMMAND_WALL));
+        assertThatCommandTypeFor("Alice wall", is(COMMAND_WALL));
     }
     
     @Test
     public void a_line_that_contains_no_keywords_a_read_command() {
-        String line = "Alice";
+        assertThatCommandTypeFor("Alice", is(COMMAND_READ));
+    }
+    
+    //~~~~~~~~ Production methods
+    
+    protected String getCommandType(String line) {
+        String command = COMMAND_READ;
         
-        String type = "";
-        if(!line.contains(KEYWORD_ARROW) && 
-            !line.contains(KEYWORD_FOLLOWS) && 
-            !line.contains(KEYWORD_WALL)) {
-            type = COMMAND_READ;
+        if(line.contains(KEYWORD_ARROW)) {
+            command = COMMAND_POST;
+        }
+        if(line.contains(KEYWORD_FOLLOWS)) {
+            command = COMMAND_FOLLOW;
+        }
+        if(line.contains(KEYWORD_WALL)) {
+            command = COMMAND_WALL;
         }
         
-        assertThat(type, is(COMMAND_READ));
+        return command;
+    }
+    
+    enum CommandType {
+        POST,
+        FOLLOW,
+        WALL,
+        READ;
+    }
+    
+    //~~~~~~~~~ Private assertions
+    
+    private <T> void assertThatCommandTypeFor(String line, Matcher<String> matcher) {
+        String commandType = getCommandType(line);
+        assertThat(commandType, matcher);
     }
 }
